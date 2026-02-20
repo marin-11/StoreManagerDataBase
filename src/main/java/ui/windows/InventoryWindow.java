e5e30ec12003fada2f1c275fc2a51250b305257f
E-Marin <ethanmarin@hunschool.org>
2026-02-20 13:01:00 -0500

    updated windows

--- src/main/java/ui/windows/InventoryWindow.java
+++ src/main/java/ui/windows/InventoryWindow.java
@@ -1,4 +1,28 @@
 package ui.windows;
 
-public class InventoryWindow {
+import com.googlecode.lanterna.gui2.*;
+import ui.UIController;
+
+public class InventoryWindow extends BasicWindow {
+
+    public InventoryWindow(UIController controller) {
+        super("Inventory");
+
+        Panel panel = new Panel();
+        panel.setLayoutManager(new LinearLayout(Direction.VERTICAL));
+
+        panel.addComponent(new Label("Inventory Items Here"));
+
+        panel.addComponent(new Button("Create Order", () -> {
+            controller.showCreateOrderPage();
+            this.close();
+        }));
+
+        panel.addComponent(new Button("Back", () -> {
+            controller.showMainMenu();
+            this.close();
+        }));
+
+        setComponent(panel);
+    }
 }
--- src/main/java/ui/windows/MainWindow.java
+++ src/main/java/ui/windows/MainWindow.java
@@ -3,46 +3,21 @@ package ui.windows;
 import com.googlecode.lanterna.gui2.*;
 import ui.UIController;
 
-import java.util.List;
-
 public class MainWindow extends BasicWindow {
 
-    private final UIController ui;
+    public MainWindow(UIController controller) {
+        super("Main Menu");
 
-    public MainWindow(UIController ui, String title) {
-        super(title);
-        this.ui = ui;
-        setHints(List.of(Window.Hint.CENTERED, Hint.EXPANDED, Hint.NO_POST_RENDERING));
-        setComponent(build());
-    }
-
-    public MainWindow(UIController ui) {
-        this(ui, "Main Menu");
-    }
-
-    private record MenuItem(String name, Runnable func) {
-    }
-
-    private Component build() {
         Panel panel = new Panel();
-        panel.setLayoutManager(
-                new LinearLayout(Direction.VERTICAL)
-        );
+        panel.setLayoutManager(new LinearLayout(Direction.VERTICAL));
 
-        MenuItem[] menu = {
-                new MenuItem("Search Questions by Tag", this::NoOp),
-                new MenuItem("Add New Question", this::NoOp),
-                new MenuItem("View All Questions", ui::showAllQuestionsPage),
-                new MenuItem("Exit", ui::closeApp)
-        };
+        panel.addComponent(new Button("View Inventory", () -> {
+            controller.showInventoryPage();
+            this.close();
+        }));
 
-        for (MenuItem mi : menu) {
-            panel.addComponent(new Button(mi.name, mi.func));
-        }
+        panel.addComponent(new Button("Exit", controller::closeApp));
 
-        return panel;
-    }
-
-    private void NoOp() {
+        setComponent(panel);
     }
 }
