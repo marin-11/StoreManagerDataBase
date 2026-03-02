16e7265b83ef9c0d76716884e3e7e23068910624
E-Marin <ethanmarin@hunschool.org>
2026-03-01 21:21:54 -0500

    init SQL

--- sqlScripts/init.sql
+++ sqlScripts/init.sql
@@ -1,31 +1,21 @@
---CREATE TABLE inventory(
---    item_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
---    item_name TEXT NULL,
---
---
---)
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS inventory;
 
-CREATE TABLE question (
-	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	question_text TEXT NOT NULL
+CREATE TABLE inventory (
+    item_id INTEGER PRIMARY KEY,
+    item_name TEXT NOT NULL,
+    item_price REAL NOT NULL,
+    current_stock INTEGER NOT NULL,
+    discount REAL
 );
 
-CREATE TABLE answer_option (
-	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	option_text TEXT NOT NULL,
-	is_answer INTEGER NOT NULL CHECK (is_answer IN (0, 1)) DEFAULT 0,
-	question_id INTEGER NOT NULL
-);
+CREATE TABLE orders (
+    order_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    item_id INTEGER NOT NULL,
+    amount_ordered INTEGER NOT NULL,
+    order_date TEXT NOT NULL,
 
-CREATE TABLE question_tag (
-	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	name TEXT NOT NULL UNIQUE
-);
-
-CREATE TABLE question_question_tag (
-	question_id INTEGER NOT NULL,
-	question_tag_id INTEGER NOT NULL,
-	PRIMARY KEY (question_id, question_tag_id),
-	CONSTRAINT question_question_tag_question_FK FOREIGN KEY (question_id) REFERENCES question(id),
-	CONSTRAINT question_question_tag_question_tag_FK FOREIGN KEY (question_tag_id) REFERENCES question_tag(id)
+    FOREIGN KEY (item_id) REFERENCES inventory(item_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
